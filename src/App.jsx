f268c03c8e636fed15e2c03d658b513bec2390d8
Matias Benary <matiasbenary@gmail.com>
2022-05-07 09:50:45 -0300

    :pencil: Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return <>
+    <Navbar />
+    <Box p={4}>Main Content Here</Box>
+  </>
+}
+
+export default App
